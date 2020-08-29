5cf18a9fcc8510b0cb920e2e33163dab7abb50f1
YuvalRotman46 <Yrotman2604@gmail.com>
2020-08-29 14:15:11 +0300

    Start developing the sockets stuff and I have alredy tedted it(seems that it's working).

--- main.asm
+++ main.asm
@@ -2,13 +2,18 @@
 section .data
     string db "Hello world !", 10, 0
 
+section .bss
+    socketfd resw 1
+
 section .data
     global _start
 
     %include "std/stdio.inc"
+    %include "sys/syscalls-sockets.inc"
 
     _start:
         printf string
+        sys_socket AF_INET, SOCK_STREAM, 0, socketfd
 
     exit:
         sys_exit 0
