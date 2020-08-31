50b5a7d1de0ca79a759cc11510c46a84372bd312
YuvalRotman46 <Yrotman2604@gmail.com>
2020-08-31 15:50:09 +0300

    Working on connect function and designing the struct sockddr_in and its alloctioning locally in stack. In main I checked those extra features and I have tried to write into the socket connection to pass data to host through the connection

--- main.asm
+++ main.asm
@@ -3,19 +3,30 @@ section .data
     string db "The socket file descriptor is : ", 0
 
 section .bss
-    socketfd resw 1
+    socketfd resq 1
+    sockaddr_in_pointer resq 1
 
 section .data
     global _start
 
     %include "std/stdio.inc"
     %include "sys/syscalls-sockets.inc"
+    %include "sys/syscalls-std.inc"
 
     _start:
-        printf string
         sys_socket AF_INET, SOCK_STREAM, 0, socketfd
+        
+        printf string
         printfn [socketfd]
         new_line
 
+        allocate_sockaddr_in AF_INET, 4444, 0
+        mov [sockaddr_in_pointer], rax
+        
+        sys_connect [socketfd], [sockaddr_in_pointer]
+
+        sys_write [socketfd], string, 10
+       
+        
     exit:
         sys_exit 0
